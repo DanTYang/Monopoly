3f5bac2fa6242221a42d19c42438379a0904e3da
Daniel Yang <yang.daniel2000@gmail.com>
2018-10-17 20:58:48 -0400

    yeet

--- src/MonoTester.java
+++ src/MonoTester.java
@@ -1,15 +1,20 @@
-
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 public class MonoTester 
 {
 	public static void main (String[] args)
 	{
-		DiceRoller a = new DiceRoller();
-		System.out.println(a.diceA());
-		System.out.println(a.diceB());
-		System.out.println(a.total());
-		a.rerollDice();
-		System.out.println(a.diceA());
-		System.out.println(a.diceB());
-		System.out.println(a.total());
+		Scanner a = new Scanner(System.in);
+		DiceRoller Dice = new DiceRoller();
+		Chance ChanceDeck = new Chance();
+		CommunityChests CC = new CommunityChests();
+		Queue Players
+		System.out.println("How Many Players?");
+		int playerAmount = a.nextInt();
+		for(int i = 0; i < playerAmount; i++)
+		{
+			
+		}
 	}
 }
